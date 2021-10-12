70e2c260ca36572f2acfbff56214b058c4e1f680
Robby Gustian <robbygustian11@gmail.com>
2021-10-12 14:11:59 +0700

    sesi 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,51 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welocme');
+});
+
+Route::get('/hello', function () {
+    echo "Hello World";
+});
+
+Route::get('/name/{name}', function ($nama) {
+    return "Hello " . $nama;
+});
+
+Route::get('/data/{data?}', function ($data = "Kosong") {
+    return "Isi Parameter = " . $data;
+});
+
+Route::get('/robby', function () {
     return view('welcome');
+});
+
+Route::get('/nama/{name}', function ($name) {
+    return 'Hello ' . $name;
+})->where('name', '[A-Za-z]+');
+
+Route::get('studiKasus/{nrp}/{nama}', function ($nrp, $nama) {
+    return $nrp . ' ' . $nama;
+})->where(['nrp' => '[0-9]+', 'nama' => '[A-Za-z]+']);
+
+Route::get('/cekbilangan/{bilangan}', function ($bilangan) {
+    if ($bilangan % 2 == 0) {
+        return "Bilangan genap";
+    } else {
+        return "Bilangan ganjil";
+    }
+});
+
+Route::get('deretBilangan/{bilangan}', function ($bilangan) {
+    for ($i = 1; $i <= $bilangan; $i++) {
+        if ($bilangan % 2 == 0) {
+            if ($i % 2 == 0) {
+                echo $i . '<br>';
+            }
+        } else {
+            if ($i % 2 != 0) {
+                echo $i . '<br>';
+            }
+        }
+    }
 });
